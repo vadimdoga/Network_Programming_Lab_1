85fe13b77a97a520405d0488bbb43ad866a176e7
Vadim <vadim.doga1@gmail.com>
2019-09-19 17:45:26 +0300

    firt version of the laboratory

---
+++ lab1.py
@@ -0,0 +1,56 @@
+import requests
+import json
+from requests.exceptions import HTTPError
+
+
+def getRequest(route, header):
+    name = requests.request("GET", url + route, headers=header)
+    return name
+
+
+def getRoute(res_name, route_name):
+    route_text = res_name.text
+    data = json.loads(route_text)
+    if 'link' not in data:
+        print("no link")
+    else:
+        link = data["link"]
+        for key, value in link.items():
+            route_name.append(value)
+    return routes
+
+
+def makeFile(file_text,file_name):
+    f = open(file_name, "w")
+    f.write(file_text.text)
+    f.close()
+
+
+url = "http://localhost:5000"
+routes = []
+
+res_register = requests.request("GET", url + "/register")
+# get access_token from json response
+json_register = res_register.text
+data = json.loads(json_register)
+x_access_token = data["access_token"]
+link = data["link"]
+header = {
+    'X-Access-Token': x_access_token
+}
+
+# home route
+res_home = getRequest(link, header)
+getRoute(res_home, routes)
+# all routes
+route1 = []
+route2 = []
+route3 = []
+route4 = []
+route1.append(routes.pop(0))
+
+for route in route1:
+    new_route = getRequest(route, header)
+    makeFile(new_route,route.replace("/",""))
+    getRoute(new_route, route1)
+print(routes)
