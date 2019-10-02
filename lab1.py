47357de4ad343f7e720103afdd0c0052ddd716e7
Vadim <vadim.doga1@gmail.com>
2019-10-02 13:07:07 +0300

    final version of Thread Pool

--- lab1.py
+++ lab1.py
@@ -1,28 +1,31 @@
 import requests
 import time
+from time import sleep
 import json
 import threading
+import queue
 import concurrent.futures
 from requests.exceptions import HTTPError
 
-
+counter = 2
 def getRequest(route, header):
     name = requests.request("GET", url + route, headers=header)
-
-    return name
-
-
-def getRoute(res_name, route_name):
+    global counter
+    counter += 1
+    getRoute(name)
+    
+def getRoute(res_name):
     route_text = res_name.text
     data = json.loads(route_text)
     if 'link' in data:
         link = data["link"]
         for key, value in link.items():
-            route_name.append(value)
+            routes.put(value)
+            print(value)
+            new_routes_list.append(value)
     else:
         print("no more links")
-
-    return route_name
+    return routes
 
 
 def makeFile(file_text, file_name):
@@ -32,46 +35,33 @@ def makeFile(file_text, file_name):
 
 
 start = time.time()
-
+new_routes_list = []
 url = "http://localhost:5000"
-routes = []
+routes = queue.Queue(maxsize=20)
 
-res_register = requests.request("GET", url + "/register")
-# get access_token from json response
-json_register = res_register.text
-data = json.loads(json_register)
-x_access_token = data["access_token"]
-link = data["link"]
-header = {
-    'X-Access-Token': x_access_token
-}
+with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+    # register
+    res_register = requests.request("GET", url + "/register")
+    # get access_token from json response
+    json_register = res_register.text
+    data = json.loads(json_register)
+    x_access_token = data["access_token"]
+    link = data["link"]
+    header = {
+        'X-Access-Token': x_access_token
+    }
 
-# home route
-res_home = getRequest(link, header)
-getRoute(res_home, routes)
-# all routes
-route1 = []
-route2 = []
-route3 = []
-route4 = []
-route1.append(routes.pop(0))
-route2.append(routes.pop(0))
-route3.append(routes.pop(0))
-route4.append(routes.pop(0))
+    # home route
+    getRequest(link, header)
 
+    for route in iter(routes.get, None):
+        executor.submit(getRequest, route, header)
+        print(counter)
+        print(len(new_routes_list))
+        if(routes.empty()):
+            if counter == len(new_routes_list):
+                break
 
-def all_routes(route_list):
-    for route in route_list:
-        new_route = getRequest(route, header)
-        makeFile(new_route, route.replace("/", ""))
-        getRoute(new_route, route_list)
-
-
-with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-    executor.submit(all_routes, route1)
-    executor.submit(all_routes, route2)
-    executor.submit(all_routes, route3)
-    executor.submit(all_routes, route4)
-
+    
 end = time.time()
 print(f"Done in {end - start}")
