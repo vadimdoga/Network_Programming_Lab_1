0b06798d7ed5478ccdc341c891f158d79c37d64e
Vadim <vadim.doga1@gmail.com>
2019-10-02 14:49:29 +0300

    Thread pool,converters,common json file

--- lab1.py
+++ lab1.py
@@ -1,10 +1,11 @@
 import requests
 import time
-import json
-import threading
+import queue
 import concurrent.futures
-import yaml
 import os
+import json
+import yaml
+import csv
 from xmljson import Abdera
 from xml.etree.ElementTree import fromstring
 from collections import OrderedDict
@@ -21,14 +22,23 @@ def csv_to_json(data):
     f.write(data)
     f.close()
 
+    csv_file = open('file.csv', 'r')
+
+    reader_csv = csv.DictReader( csv_file)
+    out = json.dumps( [ row for row in reader_csv ] )
+    return out
+def yaml_to_json(data):
+    out = json.dumps(yaml.load(data))
+    return out
 
 def getRequest(route, header):
     name = requests.request("GET", url + route, headers=header)
-
-    return name
-
-
-def getRoute(res_name, route_name):
+    global counter
+    counter += 1
+    getRoute(name)
+    makeFile(name)
+    
+def getRoute(res_name):
     route_text = res_name.text
     data = json.loads(route_text)
     if 'link' in data:
@@ -56,55 +66,36 @@ def makeFile(file_text):
                 json_result = yaml_to_json(all_data)
         else:
             json_result = all_data
-
-            route_name.append(value)
     else:
-        print("no more links")
-
-    return route_name
-
-
-def makeFile(file_text, file_name):
-    f = open(file_name, "w")
-    f.write(file_text.text)
-    f.close()
-
+        print("No data in file")
 
 start = time.time()
-
+new_routes_list = []
 url = "http://localhost:5000"
-routes = []
 
-res_register = requests.request("GET", url + "/register")
-# get access_token from json response
-json_register = res_register.text
-data = json.loads(json_register)
-x_access_token = data["access_token"]
-link = data["link"]
-header = {
-    'X-Access-Token': x_access_token
-}
+routes = queue.Queue(maxsize=20)
 
-# home route
-res_home = getRequest(link, header)
-getRoute(res_home, routes)
-# all routes
-route1 = []
-route2 = []
-route3 = []
-route4 = []
-route1.append(routes.pop(0))
-route2.append(routes.pop(0))
-route3.append(routes.pop(0))
-route4.append(routes.pop(0))
+with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+    # register
+    res_register = requests.request("GET", url + "/register")
+    # get access_token from json response
+    json_register = res_register.text
+    data = json.loads(json_register)
+    x_access_token = data["access_token"]
+    link = data["link"]
+    header = {
+        'X-Access-Token': x_access_token
+    }
 
+    # home route
+    getRequest(link, header)
 
-def all_routes(route_list):
-    for route in route_list:
-        new_route = getRequest(route, header)
-        makeFile(new_route, route.replace("/", ""))
-        getRoute(new_route, route_list)
+    for route in iter(routes.get, None):
+        executor.submit(getRequest, route, header)
+        if(routes.empty()):
+            if counter == len(new_routes_list):
+                break
 
-f = open("file.json","a")
-f.write("]")
-f.close()
+    
+end = time.time()
+print(f"Done in {end - start}")
